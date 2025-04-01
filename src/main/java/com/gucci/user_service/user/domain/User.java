fdd46494aed1d1e52c1f29ef843a470b2a93a5b6
문승주 <moonseungju@munseungjuui-MacBookPro.local>
2025-04-01 15:39:27 +0900

    Feat: User Entity 생성

---
+++ src/main/java/com/gucci/user_service/user/domain/User.java
@@ -0,0 +1,19 @@
+package com.gucci.user_service.user.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id", nullable = false)
+    private Long user_id;
+
+}
